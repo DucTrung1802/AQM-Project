e42df531617feed051fc7a702b5a1d9ced4373e6
Ly Duc Trung <lyductrungmoney@gmail.com>
2022-05-10 15:08:09 +0700

    #10 complete display DHT11 in blynk

--- blynk_read_data/blynk_read_data.ino
+++ blynk_read_data/blynk_read_data.ino
@@ -16,22 +16,23 @@
 // Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
 // See the Device Info tab, or Template settings
 
-// BLYNK LIBRARIES
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
+
 
 // DHT11 LIBRARIES
 #include "DHT.h"
 
 // PMS7003 LIBRARIES
 
-
 // BLYNK PARAMTERS
+#define BLYNK_TEMPLATE_ID "TMPLxxxxxx"
+#define BLYNK_DEVICE_NAME "ESP8266"
+#define BLYNK_AUTH_TOKEN "JB5gv1liwyq91VWaxzwxcSavHcEYt49-"
 // Comment this out to disable prints and save space
-// #define BLYNK_PRINT Serial
-#define BLYNK_TEMPLATE_ID           "TMPLxxxxxx"
-#define BLYNK_DEVICE_NAME           "ESP8266"
-#define BLYNK_AUTH_TOKEN            "JB5gv1liwyq91VWaxzwxcSavHcEYt49-"
+#define BLYNK_PRINT Serial
+
+// BLYNK LIBRARIES (MUST BE INCLUDE AFTER #define BLYNK)
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 
 char auth[] = BLYNK_AUTH_TOKEN;
 // Your WiFi credentials.
@@ -40,17 +41,20 @@ char ssid[] = "Quang Duc";
 char pass[] = "tranquangduc173";
 
 // DHT11 PARAMETERS
-#define DHTPIN 5 // what digital pin we're connected to
-#define DHTTYPE DHT11 // DHT 11
+#define DHTPIN 5       // what digital pin we're connected to
+#define DHTTYPE DHT11  // DHT 11
 DHT dht(DHTPIN, DHTTYPE);
-volatile long dht11_timer = 0;
-const int dht11_period = 1000; // ms
+long dht11_timer = millis();
+const int dht11_period = 1000;  // ms
 float humidity = 0;
 float temp_c = 0;
 float temp_f = 0;
 
 void setupBlynk() {
   Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
 }
 
 void setupDHT11() {
@@ -61,46 +65,45 @@ void readDHT11() {
   if (millis() - dht11_timer >= dht11_period) {
     // Reading temperature or humidity takes about 250 milliseconds!
     // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-    float humidity = dht.readHumidity();
+    humidity = dht.readHumidity();
     // Read temperature as Celsius (the default)
-    float temp_c = dht.readTemperature();
+    temp_c = dht.readTemperature();
     // Read temperature as Fahrenheit (isFahrenheit = true)
-    float temp_f = dht.readTemperature(true);
-    }
-
+    temp_f = dht.readTemperature(true);
     // Check if any reads failed and exit early (to try again).
-    if (isnan(h) || isnan(t) || isnan(f)) {
+    if (isnan(humidity) || isnan(temp_c) || isnan(temp_f)) {
       Serial.println("Failed to read from DHT sensor!");
       return;
     }
 
     dht11_timer = millis();
+  }
 }
 
 void displayDHT11() {
-  Blynk.virtualWrite(V1, h);
-  Blynk.virtualWrite(V2, t);
-  Blynk.virtualWrite(V3, f);
+  Blynk.virtualWrite(V1, humidity);
+  Blynk.virtualWrite(V2, temp_c);
+  Blynk.virtualWrite(V3, temp_f);
+  // Serial.print("Humidity: ");
+  // Serial.print(humidity);
+  // Serial.print(" %\t");
+  // Serial.print("Temperature: ");
+  // Serial.print(temp_c);
+  // Serial.print(" *C ");
+  // Serial.println();
 }
 
-void setup()
-{
+void setup() {
   // Debug console
   Serial.begin(115200);
-  // You can also specify server:
-  //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
-  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
-
   setupBlynk();
   setupDHT11();
-
 }
 
-void loop()
-{
+void loop() {
   Blynk.run();
   readDHT11();
-
+  displayDHT11();
   // You can inject your own code or combine it with other sketches.
   // Check other examples on how to communicate with Blynk. Remember
   // to avoid delay() function!
