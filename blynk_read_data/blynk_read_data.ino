f59559590e22348835db36437501fcd47a7ab12f
Ly Duc Trung <lyductrungmoney@gmail.com>
2022-05-10 15:50:15 +0700

    #11 complete display DHT11 and PMS7003 on Blynk

--- blynk_read_data/blynk_read_data.ino
+++ blynk_read_data/blynk_read_data.ino
@@ -16,12 +16,12 @@
 // Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
 // See the Device Info tab, or Template settings
 
-
-
 // DHT11 LIBRARIES
 #include "DHT.h"
 
 // PMS7003 LIBRARIES
+#include <SoftwareSerial.h>
+#include "PMS.h"
 
 // BLYNK PARAMTERS
 #define BLYNK_TEMPLATE_ID "TMPLxxxxxx"
@@ -44,11 +44,21 @@ char pass[] = "tranquangduc173";
 #define DHTPIN 5       // what digital pin we're connected to
 #define DHTTYPE DHT11  // DHT 11
 DHT dht(DHTPIN, DHTTYPE);
-long dht11_timer = millis();
+volatile float humidity = 0;
+volatile float temp_c = 0;
+volatile float temp_f = 0;
+volatile unsigned long dht11_timer = millis();
 const int dht11_period = 1000;  // ms
-float humidity = 0;
-float temp_c = 0;
-float temp_f = 0;
+
+// PMS7003 PARAMETERS
+SoftwareSerial serial_ESP(D2, D3);  // D2 = RX -- D3 = TX
+PMS pms(serial_ESP);
+PMS::DATA data;
+volatile unsigned int pm1_0 = 0;
+volatile unsigned int pm2_5 = 0;
+volatile unsigned int pm10_0 = 0;
+volatile unsigned long pms7003_timer = 0;
+const int pms7003_period = 1000;  // ms
 
 void setupBlynk() {
   Blynk.begin(auth, ssid, pass);
@@ -93,18 +103,48 @@ void displayDHT11() {
   // Serial.println();
 }
 
+void setupPMS7003() {
+  pinMode(D2, INPUT);
+  pinMode(D3, OUTPUT);
+  serial_ESP.begin(9600);
+}
+
+void readPMS7003() {
+  if (millis() - pms7003_timer >= pms7003_period) {
+    if (pms.read(data)) {
+    pm1_0 = data.PM_AE_UG_1_0;
+    pm2_5 = data.PM_AE_UG_2_5;
+    pm10_0 = data.PM_AE_UG_10_0;
+    }
+
+    pms7003_timer = millis();
+  }
+}
+
+void displayPMS7003() {
+  Blynk.virtualWrite(V4, pm1_0);
+  Blynk.virtualWrite(V5, pm2_5);
+  Blynk.virtualWrite(V6, pm10_0);
+}
+
 void setup() {
   // Debug console
   Serial.begin(115200);
   setupBlynk();
   setupDHT11();
+  setupPMS7003();
 }
 
-void loop() {
+void loop() {  
   Blynk.run();
-  readDHT11();
-  displayDHT11();
   // You can inject your own code or combine it with other sketches.
   // Check other examples on how to communicate with Blynk. Remember
   // to avoid delay() function!
+
+  readDHT11();
+  displayDHT11();
+
+  readPMS7003();
+  displayPMS7003();
+  
 }
